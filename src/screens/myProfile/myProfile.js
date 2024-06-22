63446fb0070473637bab1340065bbc819beea584
alawllm <alicja.willam@gmail.com>
2024-06-22 14:46:46 +0200

    inactive toggler, light syntax refactor

--- src/screens/myProfile/myProfile.js
+++ src/screens/myProfile/myProfile.js
@@ -2,7 +2,12 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Text, Image, View, StyleSheet } from "react-native";
 import { globalStyles } from "../../shared/styles";
 import { userData } from "../../shared/data";
-import { EyeIcon, LogoutIcon } from "../../../assets/icons/EyeIcon";
+import {
+  EyeIcon,
+  LogoutIcon,
+  ToggleOffIcon,
+  ToggleOnIcon,
+} from "../../../assets/icons";
 
 const data = userData[0];
 
@@ -10,6 +15,7 @@ export const MyProfileScreen = () => {
   return (
     <SafeAreaView style={[globalStyles.container, { gap: 25 }]}>
       <Text style={globalStyles.boldTextLarge}>My profile</Text>
+      {/* profile pic container  */}
       <View style={globalStyles.horizontalContainer}>
         <Image
           style={globalStyles.imgRound}
@@ -22,13 +28,20 @@ export const MyProfileScreen = () => {
           <Text style={globalStyles.regularTextSmall}>{data.email}</Text>
         </View>
       </View>
+
       <View style={[globalStyles.container, { gap: 10 }]}>
-        <View style={styles.textWithIcon}>
-          <View style={[styles.circleIcon, { backgroundColor: "#d3d3d3" }]}>
-            <EyeIcon size={26} color="black" />
+        {/* dark mode container  */}
+        <View style={styles.endsWrapper}>
+          <View style={styles.textWithIcon}>
+            <View style={[styles.circleIcon, { backgroundColor: "#d3d3d3" }]}>
+              <EyeIcon size={26} color="black" />
+            </View>
+            <Text style={globalStyles.regularTextSmall}>Dark Mode</Text>
           </View>
-          <Text style={globalStyles.regularTextSmall}>Dark Mode</Text>
+          <ToggleOffIcon size={50} color="#c6c6c6" />
         </View>
+
+        {/* logout container  */}
         <View style={styles.textWithIcon}>
           <View style={[styles.circleIcon, { backgroundColor: "#ffcccb" }]}>
             <LogoutIcon size={26} color="red" />
@@ -47,7 +60,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start",
     gap: 15,
-    marginBottom: 10,
+    marginBottom: 15,
+    width: "50%",
+  },
+  endsWrapper: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   circleIcon: {
     height: 48,
--- src/shared/styles/globalStyles.js
+++ src/shared/styles/globalStyles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 export const globalStyles = StyleSheet.create({
   container: {
     padding: 20,
-    gap: 5,
+    gap: 7,
     height: "auto",
   },
   horizontalContainer: {
